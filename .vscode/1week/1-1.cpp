c05fd4d3659a7ca6f33c22882449a392e74faefc
Heongilee <gjsrl1@gmail.com>
2020-09-17 17:46:38 +0900

    My first init

---
+++ .vscode/1week/1-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int a = 3;
+    string b = "";
+
+    cout << a << endl;
+    cout << b << endl;
+
+    return 0;
+}
